736a5ff55408bde77ac1384dc9394bf8465c39a1
unknown <BT_3W2_32@BKTECH.LOCAL>
2022-11-02 09:20:59 -0400

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        int p = 5;
+        int q = 0;
+
+        if (p > 0 && (p / q) == 2)
+        {
+            System.out.println("it’s true :)");
+        }
+        else
+        {
+            System.out.println("it’s false :(");
+        }
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
